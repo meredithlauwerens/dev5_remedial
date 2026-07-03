1455c16e4c95a0d27bb6209be36c888afe96f820
meredithlauwerens <meredith.lauwerens@student.ehb.be>
2026-07-03 14:08:10 +0200

    Backend connected to PostgreSQL

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./config/database.js";
 
 dotenv.config();
 
@@ -9,10 +10,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+// Test database connection
+pool.query("SELECT NOW()")
+  .then((result) => {
+    console.log("Connected to PostgreSQL");
+    console.log(result.rows[0]);
+  })
+  .catch((err) => {
+    console.error("Database connection failed");
+    console.error(err);
+  });
+
+
 app.get("/", (req, res) => {
-  res.send("Backend is running");
+res.send("Backend is running");
 });
 
+//port
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
